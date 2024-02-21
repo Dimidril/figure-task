d3a4e50c233c1d5676eef504bcfd18ce57d19a1c
Dmitry <proctvvv@gmail.com>
2024-02-21 20:43:40 +0300

    Подправил тесты

--- FigureTest/UnitTest1.cs
+++ FigureTest/UnitTest1.cs
@@ -1,8 +1,6 @@
 using FigureLib;
-using NUnit.Framework;
 
 namespace FigureTest;
-
 
 public class FigureTest
 {
@@ -10,23 +8,23 @@ public class FigureTest
     public void CircleAreaTest()
     {
         var circle = new Circle(5);
-        Assert.AreEqual(Math.PI * 25, circle.GetArea(), 0.001);
+        Assert.That(circle.GetArea(), Is.EqualTo(Math.PI * 25));
     }
 
     [Test]
     public void TriangleAreaTest()
     {
         var triangle = new Triangle(3, 4, 5);
-        Assert.AreEqual(6, triangle.GetArea(), 0.001);
+        Assert.That(triangle.GetArea(), Is.EqualTo(6));
     }
 
     [Test]
     public void RightTriangleTest()
     {
         var rightTriangle = new Triangle(3, 4, 5);
-        Assert.IsTrue(rightTriangle.IsRightTriangle());
+        Assert.That(rightTriangle.IsRightTriangle(), Is.True);
 
         var nonRightTriangle = new Triangle(3, 4, 6);
-        Assert.IsFalse(nonRightTriangle.IsRightTriangle());
+        Assert.That(nonRightTriangle.IsRightTriangle(), Is.False);
     }
 }
